d4423e4b3a8490882091d8c43a3221aca8246430
prisqc <prisilla.quezadac@gmail.com>
2022-10-07 05:27:32 +0200

    Página crear cuenta hecha

---
+++ app/src/main/java/com/example/pfghiking/Crear_Cuenta.java
@@ -0,0 +1,14 @@
+package com.example.pfghiking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Crear_Cuenta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_crear_cuenta );
+    }
+}
