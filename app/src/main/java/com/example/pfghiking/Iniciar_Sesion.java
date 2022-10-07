2375772fbc28fc875127c790d225b59e384a87e8
prisqc <prisilla.quezadac@gmail.com>
2022-10-07 05:25:50 +0200

    Iniciar sesion creada

---
+++ app/src/main/java/com/example/pfghiking/Iniciar_Sesion.java
@@ -0,0 +1,14 @@
+package com.example.pfghiking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Iniciar_Sesion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_iniciar_sesion );
+    }
+}
