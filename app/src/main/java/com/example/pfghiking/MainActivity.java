22a0a409e82027229e07cd0bc3b6ef743fb8827a
prisqc <prisilla.quezadac@gmail.com>
2022-10-07 05:40:57 +0200

    xml crear cuenta

--- app/src/main/java/com/example/pfghiking/MainActivity.java
+++ app/src/main/java/com/example/pfghiking/MainActivity.java
@@ -1,14 +1,65 @@
 package com.example.pfghiking;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 public class MainActivity extends AppCompatActivity {
+    Button myButton_is;
+    Button myButton_cc;
+    private FirebaseAnalytics mFirebaseAnalytics;
+    TextView AM_textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+
+
+        // --------------- CAMBIAR DE COLOR LOS BOTONES ---------------------- //
+
+        Button myButton_is  = this.<Button>findViewById(R.id.button_iniciar_sesion_AM);
+        myButton_is.setBackgroundColor(0XFF493B37);
+
+        Button myButton_cc  = this.<Button>findViewById( R.id.button_crear_cuenta_AM );
+        myButton_cc.setBackgroundColor(0XFF493B37);
+
+
+        // ---------------  BOTONES DE INTENT ---------------------- //
+
+        myButton_is.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent is_MA = new Intent(MainActivity.this, Iniciar_Sesion.class);
+                is_MA.addFlags( Intent.FLAG_ACTIVITY_CLEAR_TOP );
+                startActivity( is_MA );
+
+            }
+        } );
+
+        myButton_cc.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cc_MA = new Intent(MainActivity.this, Crear_Cuenta.class);
+                cc_MA.addFlags( Intent.FLAG_ACTIVITY_CLEAR_TOP );
+                startActivity( cc_MA );
+            }
+        } );
+
+
+
+
+
     }
 }
