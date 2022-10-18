3dad8e45f754e9e4af6178919c24cbdc0b6d1c71
prisqc <prisilla.quezadac@gmail.com>
2022-10-19 00:47:11 +0200

    nuevo activity - CREAR RUTA

--- app/src/main/java/com/example/pfghiking/Principal.java
+++ app/src/main/java/com/example/pfghiking/Principal.java
@@ -10,5 +10,9 @@ public class Principal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_principal );
+
+        //Para ocultar el actionBar
+        getSupportActionBar().hide();
+
     }
 }
