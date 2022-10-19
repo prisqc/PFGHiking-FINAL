d43be2d9bb938994cb195746b48ac722876c65f0
prisqc <prisilla.quezadac@gmail.com>
2022-10-19 03:21:03 +0200

    instanciando objetos en clase Perfil - 1era vesión

--- app/src/main/java/com/example/pfghiking/Perfil.java
+++ app/src/main/java/com/example/pfghiking/Perfil.java
@@ -1,10 +1,23 @@
 package com.example.pfghiking;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Perfil extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseDatabase mDataBase;
+    private ModelUser user;
+    private TextView tvName;
+    private Button editarPerfil;
+    private Button crearRuta;
+    private Button misRutas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +26,25 @@ public class Perfil extends AppCompatActivity {
 
         //Para ocultar el actionBar
         getSupportActionBar().hide();
+
+
+        mAuth = FirebaseAuth.getInstance();
+
+
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        mDataBase = FirebaseDatabase.getInstance( "https://pfghiking-default-rtdb.europe-west1.firebasedatabase.app/" );
+
+
+        tvName = findViewById( R.id.TV_name_Profile);
+
+        editarPerfil = findViewById( R.id.button_editar_PE );
+        editarPerfil.setBackgroundColor(0XFF6A5F4B);
+
+        crearRuta = findViewById( R.id.button_CrearRuta_PE );
+        crearRuta.setBackgroundColor(0XFF6A5F4B);
+
+        misRutas = findViewById( R.id.button_MisRutas_PE );
+        misRutas.setBackgroundColor(0XFF6A5F4B);
 
     }
 }
