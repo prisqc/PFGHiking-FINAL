558e456aa00fa86f09edea2d3ac1e98aeff731e0
prisqc <prisilla.quezadac@gmail.com>
2022-10-19 03:29:57 +0200

    Intent de perfil a Crear Ruta en la clase Perfil - 1era vesión

--- app/src/main/java/com/example/pfghiking/Perfil.java
+++ app/src/main/java/com/example/pfghiking/Perfil.java
@@ -1,6 +1,8 @@
 package com.example.pfghiking;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -66,6 +68,21 @@ public class Perfil extends AppCompatActivity {
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        } );
+
+
+
+        //Acción para cambiar de página de Perfil a Crear Ruta
+        crearRuta.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(Perfil.this, Crear_Ruta.class);
+                i.addFlags( Intent.FLAG_ACTIVITY_CLEAR_TOP );
+                startActivity( i );
+                finish();
 
             }
         } );
