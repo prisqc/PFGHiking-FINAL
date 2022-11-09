e0853ee8fb85ae4d26c1b0e618e83776b3e3918d
prisqc <prisilla.quezadac@gmail.com>
2022-11-09 16:15:12 +0100

    se agregó un empty activity llamado Info_ruta, donde se visualiza toda la información de la ruta seleccionada

---
+++ app/src/main/java/com/example/pfghiking/Info_Ruta.java
@@ -0,0 +1,14 @@
+package com.example.pfghiking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Info_Ruta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_info_ruta );
+    }
+}
