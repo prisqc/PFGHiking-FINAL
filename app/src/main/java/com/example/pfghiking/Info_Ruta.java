ffdf698f37fcf6d4990c22a70b6977f911d4c80c
prisqc <prisilla.quezadac@gmail.com>
2022-11-09 16:23:50 +0100

    creado xml para info ruta

--- app/src/main/java/com/example/pfghiking/Info_Ruta.java
+++ app/src/main/java/com/example/pfghiking/Info_Ruta.java
@@ -1,8 +1,8 @@
 package com.example.pfghiking;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Info_Ruta extends AppCompatActivity {
 
